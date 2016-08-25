07ddfebc9ca3b4f84eaeda6b7aaa0bd592df3d11
Kasiah Butler <snipercorgii@gmail.com>
2016-08-25 13:12:36 -0700

    Updated and set up being able to Draw Geometry

--- RenderUtilities/crenderutils.cpp
+++ RenderUtilities/crenderutils.cpp
@@ -1,12 +1,50 @@
 #define GLEW_STATIC
 
 #include "GLEW\glew.h"
-#include "GLFW\glfw3.h"
 
 #include "crenderutils.h"
+#include "vertex.h"
 
-void dostuff()
+Geometry makeGeometry(struct Vertex *verts, size_t vsize, unsigned int *tris, size_t tsize)
 {
-	glfwInit();
-	glfwTerminate();
+	Geometry retVal;
+	retVal.size = tsize;
+
+	
+	//Define all the Variables
+	glCreateBuffers(1, &retVal.vbo);
+	glCreateBuffers(1, &retVal.ibo);
+	glCreateVertexArrays(1, &retVal.vao);
+
+	//Scope all the variables
+	glBindVertexArray(retVal.vao);
+	glBindBuffer(GL_ARRAY_BUFFER, retVal.vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, retVal.ibo);
+
+	//Initialize the Variables
+	glBufferData(GL_ARRAY_BUFFER, vsize * sizeof(Vertex), verts, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, tsize * sizeof(unsigned), tris, GL_STATIC_DRAW);
+
+	//Attributes
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)16);
+	
+
+	//Unscope the Variables
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	return retVal;
+}
+
+void freeGeometry(Geometry &geo)
+{
+	glDeleteBuffers(1, &geo.vbo);
+	glDeleteBuffers(1, &geo.ibo);
+	glDeleteVertexArrays(1, &geo.vao);
+	geo = { 0, 0, 0, 0 };
 }
--- RenderUtilities/crenderutils.h
+++ RenderUtilities/crenderutils.h
@@ -1,3 +1,15 @@
 #pragma once
 
-void dostuff();
+struct Geometry
+{
+	unsigned vbo, ibo, vao, size;
+
+	//Vertex Buffer Object : Array of Vertices
+	//Index Buffer Object : Array of Indicies (Triangles)
+	//Vertex Array Object : Groups the two with formatting;
+	//Size : Number of Triangles
+};
+
+Geometry makeGeometry(struct Vertex *verts, size_t vsize, unsigned int *tris, size_t tsize);
+
+void freeGeometry(Geometry &);
